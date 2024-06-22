58f4f0b9cd42b8395de0012e6f3be8348979d625
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-06-22 23:58:42 +0530

    homepage background

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './homepage.scss';
 
 export default function HomePage() {
   return (
-    <div className='homepage'>
-      <div className="textContainer"></div>
-      <div className="textContainer"></div>
+    <div className="homepage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Welcome to my portfolio!</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad
+            veritatis ipsa rerum dolores vel, a illo iusto doloribus ea labore
+            placeat pariatur sed recusandae distinctio saepe veniam sit est
+            accusantium.
+          </p>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="bgsample.jpg" alt="background Image" />
+      </div>
     </div>
-  )
+  );
 }
