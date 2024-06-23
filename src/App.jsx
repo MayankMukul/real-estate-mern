2fddbb0128d02bb61c4c832f769b263133b9a630
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-06-23 23:15:26 +0530

    homepage added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react';
 import './layout.scss';
 import Navbar from './components/Navbar/Navbar';
-
+import HomePage from './routes/HomePage.jsx';
 function App() {
  
   return (
     <div className='layout'>
+      <div className="navbar">
       <Navbar></Navbar>
+      </div>
+      <div className="content">
+        <HomePage></HomePage>
+      </div>
     </div>
   )
 }
