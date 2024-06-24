3366bb139195fc370c4ba7baf85e0bb7e36d8aaf
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-06-24 23:57:57 +0530

    searchbar added to homepage

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './homepage.scss';
+import SearchBar from '../components/SearchBar/SearchBar';
 
 export default function HomePage() {
   return (
@@ -13,6 +14,21 @@ export default function HomePage() {
             placeat pariatur sed recusandae distinctio saepe veniam sit est
             accusantium.
           </p>
+          <SearchBar></SearchBar>
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>year of experence</h2>
+            </div>
+            <div className="box">
+              <h1>200+</h1>
+              <h2>Award and gain</h2>
+            </div>
+            <div className="box">
+              <h1>1200+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
         </div>
       </div>
       <div className="imgContainer">
