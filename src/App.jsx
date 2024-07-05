1dc9efc32a1eab5599122d2b4bc849b2e2472632
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-07-05 23:29:27 +0530

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
-import { useState } from 'react';
-import './layout.scss';
-import Navbar from './components/Navbar/Navbar';
-import HomePage from './routes/HomePage.jsx';
+
+import HomePage from './routes/homePage/HomePage.jsx';
+import Layout from './routes/layout/Layout.jsx';
+import ListPage from './routes/listPage/ListPage.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Login from './routes/login/Login.jsx';
+import SinglePage from './routes/singlePage/SinglePage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout></Layout>,
+    children:[
+      {
+        path:'/',
+        element:<HomePage></HomePage>
+      },
+      {
+        path:'/list',
+        element:<ListPage></ListPage>
+      },
+      {
+        path:'/login',
+        element:<Login></Login>
+      },
+      {
+        path:'/:id',
+        element:<SinglePage/>
+      }
+    ]
+  }
+])
 function App() {
  
   return (
-    <div className='layout'>
-      <div className="navbar">
-      <Navbar></Navbar>
-      </div>
-      <div className="content">
-        <HomePage></HomePage>
-      </div>
-    </div>
+    <RouterProvider router={router}></RouterProvider>
   )
 }
 
