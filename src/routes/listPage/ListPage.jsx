5eb6f786444b2071c83ea60cefa665b0217cc7f7
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-07-08 23:08:50 +0530

    update

--- src/routes/listPage/ListPage.jsx
+++ src/routes/listPage/ListPage.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import './listPage.scss';
 import { listData } from '../../lib/dummydata.js'
+import Filter from '../../components/filter/Filter.jsx';
+import Card from '../../components/Card/Card.jsx';
 
 export default function ListPage() {
   let data = listData;
   return (
     <div className='listPage'>listPage
     <div className="listContainer">list
-      <div className="wrapper"></div>
+      <div className="wrapper">
+        <Filter/>
+        {
+          data.map((item) => {
+            return <Card item={item} key={item.id}></Card>
+          })
+        }
+      </div>
     </div>
     <div className="mapContainer">map</div>
     </div>
