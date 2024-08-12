97bcf6c40d49a0b5fde8e41c86544cd8e2dc5c69
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-08-12 23:08:50 +0530

    chat component added

---
+++ src/components/chat/Chat.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import './chat.scss';
+
+export default function Chat() {
+  return (
+    <div className="chat">
+      <div className="messages">
+        <h1>Messages</h1>
+        <div className="message">
+          <img src="https://images.pexels.com/photos/1918291/pexels-photo-1918291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+          <span>Jhon Doe</span>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,
+            iste.
+          </p>
+        </div>
+        <div className="message">
+          <img src="https://images.pexels.com/photos/1918291/pexels-photo-1918291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+          <span>Jhon Doe</span>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,
+            iste.
+          </p>
+        </div>
+        <div className="message">
+          <img src="https://images.pexels.com/photos/1918291/pexels-photo-1918291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+          <span>Jhon Doe</span>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,
+            iste.
+          </p>
+        </div>
+        <div className="message">
+          <img src="https://images.pexels.com/photos/1918291/pexels-photo-1918291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+          <span>Jhon Doe</span>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,
+            iste.
+          </p>
+        </div>
+        <div className="message">
+          <img src="https://images.pexels.com/photos/1918291/pexels-photo-1918291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+          <span>Jhon Doe</span>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,
+            iste.
+          </p>
+        </div>
+      </div>
+      <div className="chatBox"></div>
+    </div>
+  );
+}
