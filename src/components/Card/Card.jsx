22d681f4461ba27dbc735c6e0e05785d3ff248e5
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-08-19 23:56:09 +0530

    card update

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,38 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import './card.scss';
 
 export default function Card(item) {
   return (
     <div className='card'>
       <Link className='imageContainer' to={`/${item.id}`} >
-      <img src="/sample-room.jpg" alt="Sample Image" />
+      <img src="./images/sample-room.jpg" alt="Sample Image" />
       </Link>
       <div className='textContainer'>
         <h2 className='title'>
           <Link to={`/${item.id}`}>{item.title}</Link>
         </h2>
         <p className='address'>
-          <img src="/sample-room.jpg" alt="sampleAddressImg" />
+          <img src="./images/address.jpg" alt="sampleAddressImg" />
           <span>{item.address}</span>
         </p>
         <p className='price'>₹{item.price}</p>
         <div className='bottom'>
           <div className="features">
             <div className="feature">
-              <img src="/bed.jpg" alt="Bedroom" />
+              <img src="./images/bed.jpg" alt="Bedroom" />
               <span>{item.bedroom} Bedroom</span>
             </div>
             <div className="feature">
-              <img src="/bathroom.jpg" alt="bathroom" />
+              <img src="./images/bathroom.jpg" alt="bathroom" />
               <span>{item.bathroom} Bathroom</span>
             </div>
           </div>
           <div className="icons">
             <div className="icon">
-              <img src="/save.jpg" alt="save" />
+              <img src="./images/save.jpg" alt="save" />
             </div>
             <div className="icon">
-              <img src="/chat.jpg" alt="Chat" />
+              <img src="./images/chat.jpg" alt="Chat" />
             </div>
           </div>
 
