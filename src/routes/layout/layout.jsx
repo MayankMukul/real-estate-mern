88aaaaf7f3837765a2edb60f53fc31feaa920d9b
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-08-29 23:51:16 +0530

    update

--- src/routes/layout/layout.jsx
+++ src/routes/layout/layout.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
+import './layout.scss';
 
-export default function layout() {
+export default function Layout() {
   return (
     <div className='layout'>
       <div className="navbar">
       <Navbar></Navbar>
       </div>
       <div className="content">
-        
         <Outlet></Outlet>
       </div>
     </div>
